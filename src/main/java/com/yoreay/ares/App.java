ca707e4ebf3a260a85aae058a31e1deb6b9e0386
YoReay <Yr492107643@Gmail.com>
2016-07-27 14:27:59 +0800

    添加配置文件

---
+++ src/main/java/com/yoreay/ares/App.java
@@ -0,0 +1,10 @@
+package com.yoreay.ares;
+
+/**
+ * Created by sanze on 2016/7/27.
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Ares");
+    }
+}
