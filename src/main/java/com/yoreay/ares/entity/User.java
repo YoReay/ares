606dbbe50a1aa85058253a98f4066b90d291b829
YoReay <Yr492107643@Gmail.com>
2016-07-27 19:18:02 +0800

    添加mybatis配置和mybatis-generator配置

---
+++ src/main/java/com/yoreay/ares/entity/User.java
@@ -0,0 +1,75 @@
+package com.yoreay.ares.entity;
+
+import java.util.Date;
+
+public class User {
+    private Long id;
+
+    private Date created;
+
+    private Date modified;
+
+    private Boolean isDeleted;
+
+    private String username;
+
+    private String password;
+
+    private Long deptId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public Date getModified() {
+        return modified;
+    }
+
+    public void setModified(Date modified) {
+        this.modified = modified;
+    }
+
+    public Boolean getIsDeleted() {
+        return isDeleted;
+    }
+
+    public void setIsDeleted(Boolean isDeleted) {
+        this.isDeleted = isDeleted;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public Long getDeptId() {
+        return deptId;
+    }
+
+    public void setDeptId(Long deptId) {
+        this.deptId = deptId;
+    }
+}
